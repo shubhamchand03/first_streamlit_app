c6c80795484c18a63f716df12b2c5a82bac96df8
SHUBHAM CHAND <45413687+shubhamchand03@users.noreply.github.com>
2024-01-16 11:58:39 +0530

    Update streamlit_app.py
    added 2 more texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Kale, spinach and smoothie')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
